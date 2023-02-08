260d7d62fd75475ced8107d73a1884218640a85f
Akoumia sevan <sevanakoumia@gmail.com>
2023-02-08 20:39:19 +0000

    Creation de notre splash screen avec import d'une image

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// screens
+import SplashScreen from './screens/SplashScreen';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SplashScreen/>
     </View>
   );
 }
